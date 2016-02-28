907f572659c686e67dfef76815f425bd8771fd0f
clau85 <chrisv208@gmail.com>
2016-02-28 22:51:35 +0000

    add the new and create route and update heder and footer

--- app.js
+++ app.js
@@ -22,6 +22,8 @@ var Blog = mongoose.model("Blog", blogSchema);
 app.get("/", function(req, res){
     res.redirect("/blogs");
 });
+
+//INDEX ROUTE
 app.get("/blogs", function(req, res){
     Blog. find({}, function(err, blogs){
         if(err){
@@ -31,6 +33,25 @@ app.get("/blogs", function(req, res){
         }
     });
 });
+
+//NEW ROUTE
+app.get("/blogs/new", function(req, res){
+    res.render("new");
+});
+
+//CREATE ROUTE
+app.post("/blogs", function(req, res){
+    //create blog
+    Blog.create(req.body.blog, function(err, newBlog){
+        if(err){
+            res.render("new");
+        }else{
+            //then, redirect to the index
+            res.redirect("/blogs");
+        }
+    });
+});
+
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("SERVER IS RUNNING!");
